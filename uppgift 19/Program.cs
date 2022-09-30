d4190e5b02fc0885a66b14ae17354d4bf539ad8d
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 14:57:29 +0200

    Add project files.

---
+++ uppgift 19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift3._19
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv två tal med - eller + exemepl (13-5)");
+            string I = Console.ReadLine();
+            if (I.Contains ("+"))
+            {
+                int plus = I.IndexOf ('+');
+                double tal1 = double.Parse(I[..plus]);
+                double tal2 = double.Parse(I[(plus + 1)..]);
+                double tot = tal1 + tal2; 
+                Console.WriteLine(I + "=" + tot);
+            }
+            else if (I.Contains ("-"))
+            {
+                int minus = I.IndexOf ('-');
+                double tal1 = double.Parse (I[..minus]);
+                double tal2 = double.Parse(I[(minus + 1)..]);
+                double tot = tal2 - tal1;
+                Console.WriteLine(I + "=" + tot);
+            }
+        }
+    }
+}
